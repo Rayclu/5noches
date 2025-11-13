3ddf65c4097c4debffa522b8a4fd684297c25d42
Ezel <ligockiezequiel@gmail.com>
2025-11-13 17:44:07 -0300

    INVENTARIO LOGIC INPUT TERMINADAAAAA ADEMAS YA PUEDO EXPORTAR LA LISTA DE ELEMENTOS

--- code/shor_cuts.gd
+++ code/shor_cuts.gd
@@ -1,16 +1,20 @@
 extends Node
-# a futura implementacion. solo planteo la idea
 
 signal OpenMenu
-signal OpenInventory
 signal OpenCameras
 var image 
+var InventoryActive = false
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
-		if event.keycode == 69: #e
+		if Input.is_action_just_pressed("e") && !InventoryActive: #e
+			InventoryActive=true
 			image = get_viewport().get_texture().get_image()
 			get_tree().change_scene_to_file("res://scenes/Inventory.tscn")
-			emit_signal("OpenInventory")
+			
+		elif Input.is_action_just_pressed("e") && InventoryActive:
+			get_tree().change_scene_to_file("res://Minigames/Scenes/Buttons_minigames.tscn")
+			InventoryActive=false
+			
 		if event.keycode == 4194305:#Esc
 			print(emit_signal("OpenMenu"))
 		if Input.is_action_just_pressed("Shift + C"):
